8fafe298816fdf3626c71fbe68dc20a71d4ac771
mudash-dev <ashleymudambaji@gmail.com>
2026-08-12 05:36:29 +0300

    Setup backend application

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,2 +1,18 @@
 import express from "express";
 import cors from "cors";
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/health", (_req, res) => {
+  res.json({
+    message: "Task Tracker API running successfully",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
